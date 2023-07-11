695f35ee6954f90b283e84b3a28f1c49a35d1537
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-11 19:12:37 -0300

    characters microservice - fix axios

--- microservices/characters/src/data/index.js
+++ microservices/characters/src/data/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return await axios.get("http://database:8004/Characters");
+    return await axios.get("http://database:8004/Character");
   },
   create: async () => {
     throw Error("Hubo un error en la BD al crear el personaje");
