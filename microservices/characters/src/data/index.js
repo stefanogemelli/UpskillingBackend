45cbef5b98053fb4d154b61d72ffdd24fddc0e84
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-11 19:04:17 -0300

    update data in character microservice

--- microservices/characters/src/data/index.js
+++ microservices/characters/src/data/index.js
@@ -1,8 +1,9 @@
 const characters = require("./characters.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return characters;
+    return await axios.get("http://database:8004/Characters");
   },
   create: async () => {
     throw Error("Hubo un error en la BD al crear el personaje");
